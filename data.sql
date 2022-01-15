e34708f6fb39c92a79fac4f5deef4ff1d63ab5dc
blagoi <dimitrovblagoi@gmail.ckm>
2022-01-15 06:34:42 +0300

    removed drops

--- data.sql
+++ data.sql
@@ -27,7 +27,7 @@ values ('Math Tutoring', 1),
 insert into Persons (Name, PersonType)
 values ('Dimitrov Blagoi', 'client'),
        ('Ivanov Ivan', 'expert'),
-       ('Slivov Stepan', 'client'),
+       ('Slivova Varvara', 'client'),
        ('Krivonosov Alexey', 'client'),
        ('Vronskiy Alexey', 'client'),
        ('Krylov Ivan', 'expert'),
@@ -44,21 +44,43 @@ insert into Reviews (Raiting, AuthorId, RecipientId)
 values (4, 4, 2),
        (4, 2, 4);
 
-call CreateLatLon(59.938480, 30.312481);   -- Saint Petersburg
-call CreateLatLon(55.755825, 37.617298);   -- Moscow
-call CreateLatLon(63.391522, -155.076621); -- Alaska
-call CreateLatLon(59.943663, 30.239423);   -- Some adress in Saint P.
+insert into LatLons (Lat, Lon)
+values (59.938480, 30.312481),   -- Saint Petersburg
+       (55.755825, 37.617298),   -- Moscow
+       (63.391522, -155.076621), -- Alaska
+       (59.943663, 30.239423);   -- Some adress in Saint P.
 
 insert into Regions (Name, LatLonId, CountryId)
 values ('Saint Petersburg', 1, 1),
 	   ('Moscow', 2, 1),
 	   ('Alaska', 3, 6);
 
-call CreateOrder('Необходим массаж спины. Срочно!!', 1, 4);
-call CreateOrder('Памагите с домашкой по матиматике. 8 класс.', 3, 1);
+insert into Orders 
+	(Text, ClientId, ServiceId)
+values 
+	('Необходим массаж спины. Срочно!!', 1, 4),
+	('Памагите с домашкой по матиматике. 8 класс.', 5, 1),
+	('Нужно починить кофемашину. Не кофемашинит!', 4, 7),
+	('Нужен макияж на завтра, на вечер. Готова начать завтра в 16:00.', 5, 4);
 
-call createChat(1, 7);
-call sendMessage('Привет!', 7, 1);
+insert into Chats 
+    	(PersonId1, PersonId2)
+	values 
+		(1, 7),
+		(2, 4),
+		(1, 2),
+		(5, 6),
+		(1, 5);
+
+insert into Messages 
+	(text, SenderId, ChatId)
+values 
+	('Привет!', 1, 1),
+	('Привет)', 7, 1),
+	('Что делаешь?', 1, 1),
+	('Не скажу! :РРРР', 7, 1),
+	('Здравствуйте! Я согласен починить за 2000 руб.', 2, 2),
+	('Здравствуйте! Хорошо, жду тогда вас завтра к 20:00', 4, 2);
 
 
 insert into VideoCalls (PersonId1, PersonId2, StartTime, EndTime)
@@ -67,5 +89,8 @@ values
 	(1, 7, '2020-07-09 13:10:08-07', '2020-07-09 13:26:54-07');
 
 
-call AddRegionToOrder(1, 1);
-call AddLatLonToOrder(1, 4);
+insert into RegionsOrders (OrderId, RegionId)
+		values (1, 1);
+
+insert into LatLonsOrders(OrderId, LatLonId)
+		values (1, 4);
