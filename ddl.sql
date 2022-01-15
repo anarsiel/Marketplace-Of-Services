91b051ecd2e9bcaddf10c8b7e628313ff79e127e
blagoi <dimitrovblagoi@gmail.ckm>
2022-01-15 03:53:12 +0300

    added many triggers

--- ddl.sql
+++ ddl.sql
@@ -184,7 +184,10 @@ create table Files
     unique (Location)
 );
 
+-- 
 -- Procedures
+-- 
+
 create procedure CreateOrder(_text varchar(500), _clientId int, _serviceId int)
 AS $$ 
 BEGIN 
@@ -293,6 +296,10 @@ BEGIN
 END 
 $$ LANGUAGE plpgsql;
 
+-- 
+--  Triggers
+-- 
+
 create or replace function person_from_chat()
 returns trigger as
 $$
@@ -312,6 +319,119 @@ create trigger sendMessageTrigger
   on Messages
   for each row
 execute function person_from_chat();
+
+create or replace function no_such_chat()
+returns trigger as
+$$
+begin
+    if exists (select ChatId
+		from Chats
+		where (PersonId1 = new.PersonId1 and PersonId2 = new.PersonId2) or (PersonId1 = new.PersonId2 and PersonId2 = new.PersonId1)
+	) then raise exception 'These two people already have chat together';
+    end if;
+    return new;
+end
+$$
+language plpgsql;
+
+create or replace function person_is_expert()
+returns trigger as
+$$
+begin
+    if 'expert' in (select PersonType
+		from Persons
+		where PersonId = new.PersonId
+	) then return new;
+    end if;
+    raise exception 'Only expert can take Order';
+end
+$$
+language plpgsql;
+
+create trigger takeOrderTrigger
+  before insert
+  on ExpertsOrders
+  for each row
+execute function person_is_expert();
+
+create or replace function deleteBeforeOrder()
+returns trigger as
+$$
+begin
+    delete from ExpertsOrders
+    where OrderId = old.orderId;
+
+    delete from RegionsOrders
+    where OrderId = old.orderId;
+
+    delete from LatLonsOrders
+    where OrderId = old.orderId;
+
+    delete from Files
+    where OrderId = old.orderId;
+
+    return old;
+end
+$$
+language plpgsql;
+
+create trigger deleteOrderTrigger
+  before delete
+  on Orders
+  for each row
+execute function deleteBeforeOrder();
+
+create or replace function deleteBeforePerson()
+returns trigger as
+$$
+begin
+    delete from Reviews
+    where RecipientId = old.PersonId or AuthorId = old.PersonId;
+
+    delete from Videocalls
+    where PersonId1 = old.PersonId or PersonId2 = old.PersonId;
+
+    delete from Chats
+    where PersonId1 = old.PersonId or PersonId2 = old.PersonId;
+
+    delete from Orders
+    where ClientId = old.PersonId;
+
+    delete from ExpertsOrders
+    where PersonId = old.PersonId;
+
+    return old;
+end
+$$
+language plpgsql;
+
+create trigger deletePersonTrigger
+  before delete
+  on Persons
+  for each row
+execute function deleteBeforePerson();
+
+create or replace function deleteBeforeChat()
+returns trigger as
+$$
+begin
+    delete from Messages
+    where ChatId = old.ChatId;
+
+    return old;
+end
+$$
+language plpgsql;
+
+create trigger deleteChatTrigger
+  before delete
+  on Chats
+  for each row
+execute function deleteBeforeChat();
+
+-- 
+-- Indexes
+-- 
 
 -- Foreign keys
 create index on Services using hash (ServicesGroupId);
